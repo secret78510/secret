e580bb3740ece0542e30888d166ead735758ab54
jerry <56226141+secret78510@users.noreply.github.com>
2020-11-25 00:10:52 +0800

    Add files via upload

---
+++ src/components/backend/Manage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <AlertMessage/>
+      <MagageNavbar/>
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import AlertMessage from './AlertMessage.vue';
+import MagageNavbar from './ManageNavbar.vue';
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    MagageNavbar,
+    AlertMessage,
+  },
+};
+</script>
+
+<style>
+
+</style>
